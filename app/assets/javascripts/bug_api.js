be8d8f87c48c7869f624a9bc0557585f4906e726
undersky0 <richardlonesteen@gmail.com>
2015-05-20 22:15:35 +0100

    js api

--- app/assets/javascripts/bug_api.js
+++ app/assets/javascripts/bug_api.js
@@ -60,4 +60,41 @@ $(document).ready(function() {
 
 
     console.log("create 5x5 array with options: " + createArray());
+
+    function createCORSRequest(method){
+        var apiKey = "380ce15c9ae5893acc678f0e3ec24a85";
+        var apiId = "b782df16";
+        var url = "https://www.bookingbug.com/api/v1/114784/services";
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr){
+            // XHR has 'withCredentials' property only if it supports CORS
+            xhr.open(method, url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('App-id', apiId);
+            xhr.setRequestHeader('App-key', apiKey);
+        } else if (typeof XDomainRequest != "undefined"){ // if IE use XDR
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            xhr = null;
+        }
+        return xhr;
+    }
+    var request = createCORSRequest( "get" );
+    if ( request ){
+        // Define a callback function
+        request.onload = function(){};
+        // Send request
+        request.send();
+    }
+    request.onreadystatechange=function(){
+        if (request.readyState==4 && request.status==200)
+        {
+            var a = JSON.parse(request.responseText);
+            var length = a._embedded.services.length -2;
+            while (length != a._embedded.services.length){
+                console.log(a._embedded.services[length]['name']);
+                length++;}
+        }
+    };
 });
