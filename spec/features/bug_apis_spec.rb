0de5b4db31380796145e0b0756c233e4876a1836
undersky0 <richardlonesteen@gmail.com>
2015-05-20 02:43:03 +0100

    first commit

--- spec/features/bug_apis_spec.rb
+++ spec/features/bug_apis_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "BugApis", type: :feature do
+
   describe "smoke test" do
-    it 'should not have JavaScript errors', :js => true do
+    it 'should not have any JavaScript errors', :js => true do
       visit(bug_api_index_path)
       expect(page).not_to have_errors
-      puts page.driver.console_messages #
     end
   end
+
+  describe "javascript messages" do
+    it 'prints console messages', :js => true do
+      visit(bug_api_index_path)
+      puts page.driver.console_messages
+    end
+  end
+
 end
